f3656a723809c79dd6b65c97b0e564722439a7d5
EzraKirn <dziwne.konta2@gmail.com>
2022-10-30 11:44:01 +0100

    new venv

--- Fitter/main.cpp
+++ Fitter/main.cpp
@@ -4,16 +4,10 @@
 #include <limits>
 #include <vector>
 #include <thread>
-#include "src\clenup.cpp"
-#include "src\pump.cpp"
+#include "src\UI.cpp"
 
 #define GRAVITY 9.81
 #define WATER_DENSITY 998
-
-void turn_namefiles_content_into_function(){
-    system("calculate_coeficients.py");
-}
-
 
 std::fstream& GotoLine(std::fstream& file, unsigned int num){
     file.seekg(std::ios::beg);
@@ -60,47 +54,6 @@ void generate_dataset(){
 }
 
 int main(){
-    std::string name;
-    float input[4];
-    std::cout<<"Podaj dane wejiowe"<<"\n";
-    
-    std::cout<<"Nawa pompy: ";
-    std::cin>>name;
-
-    std::cout<<"Hg: ";
-    std::cin>>input[0];
-
-    std::cout<<"Hd: ";
-    std::cin>>input[1];
-
-    std::cout<<"Q: ";
-    std::cin>>input[2];
-
-    std::cout<<"n: ";
-    std::cin>>input[2];
-
-    pump pump1(name,input[0],input[1] ,input[2] ,input[3]);     
-    pump1.store_to_namefile(pump1.create_HQ_file());
-    pump1.store_to_namefile(pump1.create_PQ_file());
-
-    std::thread python(turn_namefiles_content_into_function);
-    python.join();
-
-    //cleanup();
-    // // std::cout<<"n: ";
-    // // std::getline(std::cin, tmp);
-    // // const float n_ini=std::stof(tmp);
-
-    // // const float r=(Hd_ini-Hg_ini)/pow(Q_ini,2);
-
-    // // collect_data();
-
-    // generate_dataset();
-
-    // std::fstream file("file_names.txt");
-    // GotoLine(file,2);
-    // std::string line;
-    // file>>line;
-    // std::cout<<line;
+        show();
     return 0;
 }
